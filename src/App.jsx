6bc01938100b58e19a8c924a3345a048d559236a
Tomek <tomasz.polomski77@gmail.com>
2025-01-14 16:50:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import menu from './data';
+import Title from './Title';
+import Categories from './Categories';
+import Menu from './Menu';
+
+// const tempCategories = menu.map(item => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all', ...tempSet];
+// console.log(tempItems);
+
+const allCategories = ['all', ...new Set(menu.map(item => item.category))];
+
+const App = () => {
+	const [menuItems, setMenuItems] = useState(menu);
+	const [categories, setCategories] = useState(allCategories);
+	const filterItems = category => {
+		if (category === 'all') {
+			setMenuItems(menu);
+			return;
+		}
+		const newItems = menu.filter(item => item.category === category);
+		setMenuItems(newItems);
+	};
+
+	return (
+		<main>
+			<section className='menu'>
+				<Title text='our menu' />
+				<Categories
+					categories={categories}
+					filterItems={filterItems}
+				/>
+				<Menu menuItems={menuItems} />
+			</section>
+		</main>
+	);
+};
+export default App;
